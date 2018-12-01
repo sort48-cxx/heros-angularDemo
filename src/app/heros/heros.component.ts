d62453afbc6f892d2436feca1caf57eead785b89
sort48-cxx <747313337@qq.com>
2018-12-01 10:57:53 +0800

    angularDemo2

--- src/app/heros/heros.component.ts
+++ src/app/heros/heros.component.ts
@@ -1,18 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import {Hero} from './heros'
+import {HEROES} from './mock-heroes'
+
 @Component({
   selector: 'app-heros',
   templateUrl: './heros.component.html',
   styleUrls: ['./heros.component.css']
 })
 export class HerosComponent implements OnInit {
-  hero:Hero={
-    id:1,
-    title:'Windstorm',
-  }
+  // hero:Hero={
+  //   id:1,
+  //   name:'Windstorm',
+  // }
+  heroes = HEROES;
+  selectedHero: Hero;
   constructor() { }
 
   ngOnInit() {
   }
-
+  onSelect(hero:Hero):void{
+    console.log(hero);
+    this.selectedHero=hero;
+  }
 }
